9708908b25cc95f4cdb9ffae25bcc94d89e76af0
alebedev <alebedev@48a8c9ab-4505-49ce-ac47-c58b87ccfe91>
2012-02-13 09:00:53 +0000

    test

--- Globerry/src/main/java/com/globerry/project/dao/EventDao.java
+++ Globerry/src/main/java/com/globerry/project/dao/EventDao.java
@@ -1,6 +1,39 @@
 package com.globerry.project.dao;
 
-public class EventDao
+import java.util.List;
+
+import com.globerry.project.domain.City;
+import com.globerry.project.domain.Event;
+
+public class EventDao implements IEventDao
 {
+
+    @Override
+    public List<Event> getEventList(City city)
+    {
+	// TODO Auto-generated method stub test
+	return null;
+    }
+
+    @Override
+    public void removeEvent(Event event)
+    {
+	// TODO Auto-generated method stub
+	
+    }
+
+    @Override
+    public void addEvent(Event event, City city)
+    {
+	// TODO Auto-generated method stub
+	
+    }
+
+    @Override
+    public void updateEvent(Event oldEvent, Event newEvent)
+    {
+	// TODO Auto-generated method stub
+	
+    }
 
 }
