34a6c15aa27487818a43c77ae26ab05816042837
alebedev <alebedev@48a8c9ab-4505-49ce-ac47-c58b87ccfe91>
2012-02-13 10:58:16 +0000

    Добавил код согласно статье в EventDao.java

--- Globerry/src/main/java/com/globerry/project/dao/EventDao.java
+++ Globerry/src/main/java/com/globerry/project/dao/EventDao.java
@@ -1,31 +1,35 @@
 package com.globerry.project.dao;
 
 import java.util.List;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import com.globerry.project.domain.City;
 import com.globerry.project.domain.Event;
 
+@Repository
 public class EventDao implements IEventDao
 {
 
+    private SessionFactory sessionFactory;
     @Override
     public List<Event> getEventList(City city)
     {
-	// TODO Auto-generated method stub test
-	return null;
+	
+	// TODO Auto-generated method stub 
+	return sessionFactory.getCurrentSession().createQuery("from Event").list();
     }
 
     @Override
     public void removeEvent(Event event)
     {
-	// TODO Auto-generated method stub
-	
+	sessionFactory.getCurrentSession().delete(event);
     }
 
     @Override
     public void addEvent(Event event, City city)
     {
-	// TODO Auto-generated method stub
 	
     }
 
