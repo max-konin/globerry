5d5c050cf27e88cb61cf132a3b752f374f897b31
alebedev <alebedev@48a8c9ab-4505-49ce-ac47-c58b87ccfe91>
2012-02-14 11:31:57 +0000

    Тестировал Junit. Он работает, но не на Dao слое

--- Globerry/src/test/java/com/globerry/project/dao/CompanyTest.java
+++ Globerry/src/test/java/com/globerry/project/dao/CompanyTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import com.globerry.project.dao.ICompanyDao;
+import com.globerry.project.dao.CompanyDao;
 import com.globerry.project.domain.Company;
 
 import junit.framework.TestCase;
@@ -17,12 +19,17 @@ public class CompanyTest extends TestCase
     public void test() //throws InterruptedException
     {
 	Company test = new Company();
+	test.setName("Google");
+	test.setEmail("google@gmail.com");
+	ICompanyDao cd = new CompanyDao();
+	cd.addCompany(test);
+	System.out.print(test.getName());
 	//rangeTest.setMaxX(1);
 	//wait(2);
 	//assertEquals(1, rangeTest.getMaxX());
 	//CompanyDao dao = new CompanyDao();
 	//dao.addCompany(test);
-	fail("Not yet implemented");
+	//fail("Not yet implemented");
 	//failNotEquals("qqqq", 1, 2);
     }
 
