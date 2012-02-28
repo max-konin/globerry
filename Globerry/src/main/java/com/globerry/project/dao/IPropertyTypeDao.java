dd3f03a0ed041868a75dc093373869eb66fc4c2a
alebedev <alebedev@48a8c9ab-4505-49ce-ac47-c58b87ccfe91>
2012-02-28 09:28:33 +0000

    Добавлена обработка на уникальность unique

--- Globerry/src/main/java/com/globerry/project/dao/IPropertyTypeDao.java
+++ Globerry/src/main/java/com/globerry/project/dao/IPropertyTypeDao.java
@@ -3,11 +3,12 @@ package com.globerry.project.dao;
 import java.util.List;
 
 
+import com.globerry.project.MySqlException;
 import com.globerry.project.domain.PropertyType;
 
 public interface IPropertyTypeDao
 {
-    public void addPropertyType(PropertyType propertyType);
+    public void addPropertyType(PropertyType propertyType) throws MySqlException;
     public List<PropertyType> getPropertyTypeList();
     public void updatePropertyType(PropertyType propertyType);
     public void removePropertyType(PropertyType propertyType);
