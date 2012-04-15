43416893b5aa25775442ad291cb4e24c579cb314
skrupin <skrupin@48a8c9ab-4505-49ce-ac47-c58b87ccfe91>
2012-04-15 14:36:10 +0000

    GL-86 #comment quick fix

--- Globerry/src/main/java/com/globerry/project/service/Slider.java
+++ Globerry/src/main/java/com/globerry/project/service/Slider.java
@@ -8,6 +8,7 @@ import com.globerry.project.service.interfaces.ISlider;
 public class Slider extends Observable implements ISlider
 {
     private float state;
+    private float stateSecond;
     private PropertyType type;
     public Slider(PropertyType type){
 	this.type = type;
@@ -29,8 +30,17 @@ public class Slider extends Observable implements ISlider
     @Override
     public void onChange(float newStateLeft, float newStateRight)
     {
+	state = newStateLeft;
+	stateSecond = newStateRight;
+    }
+    public float getStateLeft()
+    {
+	return state;
+    }
+    public float getStateRight()
+    {
 	// TODO Auto-generated method stub
-	
+	return stateSecond;
     }
 
 }
