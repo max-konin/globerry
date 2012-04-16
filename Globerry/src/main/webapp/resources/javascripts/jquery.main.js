3c1b0bf9978d53fc7f8851e07dc6ac586ce1f4fe
vboiko <vboiko@48a8c9ab-4505-49ce-ac47-c58b87ccfe91>
2012-04-16 02:45:41 +0000

    Верстка.

--- Globerry/src/main/webapp/resources/javascripts/jquery.main.js
+++ Globerry/src/main/webapp/resources/javascripts/jquery.main.js
@@ -1,35 +1,62 @@
 jQuery(document).ready(function () {
 
     function mySlider(leftLimit, rightLimit, measure, div, leftInput, rightInput, pos) {
+		var leftMeasure = "";
+		var rightMeasure = "";
+		if (pos == "l")
+			leftMeasure = measure;
+		if(pos == "r")
+			rightMeasure = measure;
         jQuery("#" + div).slider({
             min: leftLimit,
             max: rightLimit,
             values: [leftLimit, rightLimit],
             range: true,
             stop: function (event, ui) {
-
-                jQuery("input#" + leftInput).val(jQuery("#" + div).slider("values", 0));
-                jQuery("input#" + rightInput).val(jQuery("#" + div).slider("values", 1));
+			if(measure == "+")
+				if((jQuery("#" + div).slider("values", 0)) > 0)
+					leftMeasure = "+";
+				else
+					leftMeasure = "";
+            jQuery("input#" + leftInput).val(leftMeasure + (jQuery("#" + div).slider("values", 0)) + rightMeasure);
+			if(measure == "+")
+				if((jQuery("#" + div).slider("values", 1)) > 0)
+					leftMeasure = "+";
+				else
+					leftMeasure = "";
+            jQuery("input#" + rightInput).val(leftMeasure + (jQuery("#" + div).slider("values", 1)) + rightMeasure);
                 //Important! values of the inputs, will change only after the previous two lines.
             },
             slide: function (event, ui) {
-                jQuery("input#" + leftInput).val(jQuery("#" + div).slider("values", 0));
-                jQuery("input#" + rightInput).val(jQuery("#" + div).slider("values", 1));
+				if(measure == "+")
+					if((jQuery("#" + div).slider("values", 0)) > 0)
+						leftMeasure = "+";
+					else
+						leftMeasure = "";
+                jQuery("input#" + leftInput).val(leftMeasure + (jQuery("#" + div).slider("values", 0)) + rightMeasure);
+				if(measure == "+")
+					if((jQuery("#" + div).slider("values", 1)) > 0)
+						leftMeasure = "+";
+					else
+						leftMeasure = "";
+                jQuery("input#" + rightInput).val(leftMeasure + (jQuery("#" + div).slider("values", 1)) + rightMeasure);
             }
         });
     };
 
-    
 
-    firstSL = new mySlider(-35, 35, 1, "tempSlider", "minCost", "maxCost", "l");
-    firstSL = new mySlider(0, 30, 1, "alchSlider", "alcMinCost", "alcMaxCost", "l");
-    firstSL = new mySlider(0, 24, 1, "timeSlider", "TimeMinV", "TimeMaxV", "l");
+
+    firstSL = new mySlider(-35, 35, "+", "tempSlider", "minCost", "maxCost", "l");
+    firstSL = new mySlider(0, 30, "$", "alchSlider", "alcMinCost", "alcMaxCost", "l");
+    firstSL = new mySlider(0, 24, " Ч", "timeSlider", "TimeMinV", "TimeMaxV", "r");
     $("#MoodSlider").slider();
-    firstSL = new mySlider(0, 300, 1, "LivSlider", "LivMinV", "LivMaxV", "l");
+    firstSL = new mySlider(0, 300, "$", "LivSlider", "LivMinV", "LivMaxV", "l");
     $("#securitySlider").slider();
-    firstSL = new mySlider(0, 100, 1, "foodSlider", "FoodMinV", "FoodMaxV", "l");
+    firstSL = new mySlider(0, 100, "$", "foodSlider", "FoodMinV", "FoodMaxV", "l");
     $("#SexSlider").slider();
-    alert('sliders have to be here');
+    $("#WhoS").selectbox().fadeOut(1000);
+    $("#WhatS").selectbox().fadeOut(1000);
+    $("#WhenS").selectbox().fadeOut(5000);
 });
 
 
--- Globerry/src/main/webapp/resources/javascripts/main.js
+++ Globerry/src/main/webapp/resources/javascripts/main.js
@@ -4,42 +4,145 @@ window.onload = function () {
         var t = event.target || event.srcElement
         alert(t.id)
     }); */
-    var headerflag = 0
+    var headerflag = 0;
+	var prevBotBut;
+	var bottomActive = false;
+    var prev = document.getElementById('jan');
     $("#SliderRequest").hide();
     $("#upperToggle").hide();
     $("#bottomToggle").hide();
 
     $("#buttonHeadSwitcher").click(function (event) {
-        if ((headerflag % 2) == 0)
+        if ((headerflag % 2) == 0) {
             $("#head").slideToggle("slow", function () {
                 $("#SliderRequest").slideToggle("slow");
                 $("#upperToggle").slideToggle("slow");
                 $("#bottomToggle").slideToggle("slow");
+				//$("bottomButton").style.background = 'rgb(37, 46, 64)';
+				$("#bottom").show();
+				//alert($(document.getElementById("WhenS")).val);
+				//alert($('select.#WhenS option:selected').option);
+				//alert($("#WhenS").val());
+				//jQuery("input#HText").val("Едем " + $("#WhoS").val() + " " + $("#WhatS").val() + " в " + $("#WhenS").val() + "!");
+				jQuery("input#HText").val("Едем " + $("#WhoS").val() + " " + $("#WhatS").val() + " в " + $("#WhenS").val() + "!");
+				//prev.style.background = 'rgb(37, 46, 64)';
+				//$()
+				//alert($("#WhenS").options[$("#WhenS").val()].id);
             });
-        else
-            $("#SliderRequest").slideToggle("slow", function () { $("#head").slideToggle("slow") });
+		}
+        else {
+				$("#SliderRequest").slideToggle("slow", function () { $("#head").slideToggle("slow") });
+				jQuery("input#HText").val("");
+				//$("#bottom").hide();
+				//$("#WhiteBottom").animate({height:0},"slow");
+			}
 
         headerflag++;
 
     });
     $("#bottomToggle").click(function (event) {
-        if ((headerflag % 2) == 0)
-            $("#head").slideToggle("slow", function () { $("#SliderRequest").slideToggle("slow") });
+        if ((headerflag % 2) == 0) {
+				$("#head").slideToggle("slow", function () { $("#SliderRequest").slideToggle("slow") });
+				$("#bottom").show();
+			}
         else {
+			jQuery("input#HText").val("");
             $("#bottomToggle").slideToggle("slow");
             $("#upperToggle").slideToggle("slow");
             $("#SliderRequest").slideToggle("slow", function () { $("#head").slideToggle("slow") });
+			$("#bottom").animate({bottom:0},"slow");
+			if(bottomActive == true) {
+				//alert(prevBotBut.id);
+				$(("#" +prevBotBut.id + "B")).hide();
+				//$(prevBotBut).style.background = 'rgb(37, 46, 64)';				
+			}
+			$("#WhiteBottom").animate({height:0},"slow");
+			$("#bottom").hide();
+			bottomActive = false;
         }
         headerflag++;
     });
     
 
-    var prev = document.getElementById('jan');
     $(".calMonth").click(function (event) {
         prev.style.background = 'rgb(37, 46, 64)';
         prev = this;
         this.style.background = 'rgb(236, 72, 7)';
-    }); 
+		//alert($("#" + this.id+"O").val());
+		jQuery("select#WhenS").val($("#" + this.id+"O").val()).attr("selected", "selected");
+		//$("select#WhenS option:selected").val($("#WhenS").val());//.attr("selected", "selected");
+		//$('select#WhenS').val($("#WhenS").val()).change();
+		jQuery("input#HText").val("Едем " + $("#WhoS").val() + " " + $("#WhatS").val() + " в " + $("#WhenS").val() + "!");
+		selectWhensInMonthes = document.getElementById('WhenS');
+		//selectWhensInMonthes.options[$("#WhenS").val()].selected=true;
+		for(i in selectWhensInMonthes.options){
+			//alert("i: "+i + " op[i]: " + selectWhensInMonthes.options[i]);
+		}
+    });
+	
+	$("#WhenS").change(function () {
+		$("select option:selected").each(function () {
+				str="";
+				if(this.id == "janO") str="jan";
+				if(this.id == "febO") str="feb";
+				if(this.id == "marO") str="mar";
+				if(this.id == "aprO") str="apr";
+				if(this.id == "mayO") str="may";
+				if(this.id == "junO") str="jun";
+				if(this.id == "julO") str="jul";
+				if(this.id == "sepO") str="sep";
+				if(this.id == "octO") str="oct";
+				if(this.id == "novO") str="nov";
+				if(this.id == "decO") str="dec";
+				if(str != ""){
+					prev.style.background = 'rgb(37, 46, 64)';
+					prev = document.getElementById(str);
+					prev.style.background = 'rgb(236, 72, 7)';
+				}
+              });
+	});
+	
+	
+	$(".bottomButton").click(function(){
+		if( bottomActive == false){
+			if(prevBotBut != undefined){
+				prevBotBut.style.background = 'rgb(37, 46, 64)';
+			}
+			prevBotBut = this;
+			$("#bottom").animate({bottom:300},"slow");
+			$("#WhiteBottom").animate({height:300},"slow", function () { $(("#" +prevBotBut.id + "B")).show(); });			
+			this.style.background = 'rgb(255, 122, 2)';
+			$(this).animate({width:124}, 0);
+			bottomActive = true;
+			prevBotBut = this;
+		} else
+		if(bottomActive == true){
+			if(prevBotBut == this){
+				$("#bottom").animate({bottom:0},"slow");
+				$(("#" +prevBotBut.id + "B")).hide();
+				$("#WhiteBottom").animate({height:0},"slow");
+				this.style.background = 'rgb(37, 46, 64)';
+				$(this).animate({width:120}, 0);
+				bottomActive = false;
+			} else
+			{
+				this.style.background = 'rgb(255, 122, 2)';
+				$(("#" +prevBotBut.id + "B")).hide();
+				$(this).animate({width:124}, 0);
+				prevBotBut.style.background = 'rgb(37, 46, 64)';
+				$(prevBotBut).animate({width:120}, 0);
+				prevBotBut = this;
+				$(("#" +prevBotBut.id + "B")).show();
+			}
+		}
+	});/**/
+	$(".bottomButton").click(function(){
+		//alert(this.id);
+	});
+	$(window).resize(function (e) { 
+		/*alert("123");*/
+	});
+	
     
     init();
 
