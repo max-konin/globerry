01142a4f12378115cb02219de54d559794f2ea95
mkonin <mkonin@48a8c9ab-4505-49ce-ac47-c58b87ccfe91>
2012-06-07 09:29:13 +0000

    PropertySegment был перенесен

--- Globerry/src/test/java/com/globerry/project/service/SlidersTest.java
+++ Globerry/src/test/java/com/globerry/project/service/SlidersTest.java
@@ -17,9 +17,10 @@ import org.springframework.test.context.support.DirtiesContextTestExecutionListe
 import org.springframework.test.context.transaction.BeforeTransaction;
 
 import com.globerry.project.dao.ContextLoaderListener;
-import com.globerry.project.dao.PropertySegment;
+
 import com.globerry.project.domain.PropertyType;
 import com.globerry.project.service.interfaces.ISlider;
+import com.globerry.project.utils.PropertySegment;
 /**
  * 
  * @author Sergey Krupin
